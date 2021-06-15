0a14b4d125f0160bcadd9ebef6f5b1f1ca793330
Alex Sheintsvit <grubian@yahoo.com>
2021-06-14 20:58:29 -0400

    Added abilty to send ACK (to a broadcast destination for now)

--- display.ino
+++ display.ino
@@ -19,17 +19,21 @@ Preferences prefs;
 //Uncomment and run this program once to erase the WiFi configuration(s). Don't forget to comment out again
 //wifiManager.resetSettings();
 
+uint8_t broadcastAddress[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
 /****************** iDefinitions shared with wearable, make sure to keep in sync *****************/
 // TODO - mode to a shared header file 
 
 #define MAX_MESSAGE 250
 unsigned char in_message[MAX_MESSAGE];
+unsigned char out_message[MAX_MESSAGE];
 
 enum message_type_e {
     SCORE,
     SCREEN_CHANGE,
     STEPS,
     HEART_RATE,
+    ACK,
+    NACK,
     /* Add new message types at the end */
 };
 
@@ -43,19 +47,44 @@ typedef struct screen_change_data_s {
   unsigned char message_type;
   unsigned char screen; // TBD
 } screen_change_data_t;
+
+static void dump_message(const uint8_t *pData, int len)
+{
+  int i;
+  for (i=0; i<len; i++) {
+    Serial.printf("%02X ", pData[i]);   
+  }
+  Serial.printf("\n");
+}
 /****************** iDefinitions shared with wearable, make sure to keep in sync *****************/
-void IRAM_ATTR OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+static void send_ack()
+{
+  out_message[0] = ACK;
+  Serial.printf("Sending: ");
+  dump_message(out_message, 1);
+  esp_now_send(broadcastAddress, (uint8_t*)out_message, 1);
+}
+
+void IRAM_ATTR OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
   //static BaseType_t xHigherPriorityTaskWoken = pdFALSE;
   //memcpy(&buttonData, incomingData, sizeof(buttonData));
   //vTaskNotifyGiveFromISR(wearMonTask_handle, &xHigherPriorityTaskWoken); 
   //portYIELD_FROM_ISR();
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
   /* 
    *  First byte in incoming data from wearable is always message type 
    *  From that we can derive the lengthof the message
    *  Or perhaps use TLV format instead ? TBD
    */
   unsigned char message_type = (unsigned char)*incomingData;
-  Serial.printf("Receied message type %d\n", message_type);
+  
+  // Dump incoming data
+  Serial.printf("Received from %s: ", macStr);
+  dump_message(incomingData, len);
+  
+  Serial.printf("Received message type %d\n", message_type);
   switch (message_type) {
     case SCORE:
       score_data_t score_msg;
@@ -74,6 +103,7 @@ void IRAM_ATTR OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int 
       Serial.printf("message type %d not supported\n", message_type);
       break;
   }
+  send_ack();
 }
 
 void setup() {
@@ -87,12 +117,19 @@ void setup() {
   //Set the WiFi to station Mode
   WiFi.mode(WIFI_STA);
   Serial.println(WiFi.macAddress());
+  WiFi.setTxPower(WIFI_POWER_MINUS_1dBm); //Minimum WiFi RF power output ~ 120mA
   //Initialize ESP-NOW
   if(esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
     //FUTURE_UPDATE: here we can have special screen on touchscreen to say WiFi error
   }
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+  esp_now_add_peer(&peerInfo);
+  Serial.printf("\tESP-NOW should be up and running\n");
 #ifdef SERIAL_ON
   Serial.println("ESP-NOW initalized");
 #endif
